a436bd994b4324b81b0cef786fea6f34ae57f315
AtamanovYS <atamanov.ys@gmail.com>
2021-04-10 11:52:43 +0000

    Подготовка начального каркаса

---
+++ src/Cli.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Differ\Cli;
+
+function run(): void
+{
+    $doc = <<<DOC
+    Generate diff
+    
+    Usage:
+      gendiff (-h|--help)
+      gendiff (-v|--version)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
+    
+    Options:
+      -h --help                     Show this screen
+      -v --version                  Show version
+      --format <fmt>                Report format [default: stylish]
+    DOC;
+
+    $args = \Docopt::handle($doc, []);
+
+    \Differ\Differ\genDiff($args['<firstFile>'], $args['<secondFile>'], $args['--format']);
+}
---
+++ src/Differ.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Differ\Differ;
+
+function genDiff(string $pathToFile1, string $pathToFile2, string $format = 'stylish'): string
+{
+    $difference = '';
+    return $difference;
+}
